47decb3e9c048f524009926aed37af8f14580282
0Mounir <m.mounir188@gmail.com>
2020-04-18 13:11:37 +0200

    Add SDL library in application

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "SDL.h"
 
 int main() {
+  // Initialize SDL
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cerr << "SDL could not initialize.\n";
+    std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
+  }
     std::cout << "Hello World!" << "\n";
     return 0;
 }
