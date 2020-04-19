65cac313fa80c28a1179468a01cb548a55e3364d
0Mounir <m.mounir188@gmail.com>
2020-04-19 11:27:58 +0200

    Pacman cannot move through walls now

--- src/pacman.cpp
+++ src/pacman.cpp
@@ -2,34 +2,57 @@
 #include <cmath>
 #include <iostream>
 
+bool Pacman::TryToMove(float x, float y){
+    for(auto it=wall.begin(); it!=wall.end(); ++it){
+        if(PacmanIntersect(x, width, it->x, 32) && PacmanIntersect(y, height, it->y, 32))
+	    return false;
+    }
+    return true;
+}
+
 void Pacman::UpdatePosition() {
+    bool update = false;
+    float new_y = pos_y;
+    float new_x = pos_x;
   switch (direction) {
     case Direction::kUp:
-      pos_y -= speed;
+        new_y -= speed;
       break;
 
     case Direction::kDown:
-        pos_y += speed;
+        new_y += speed;
       break;
 
     case Direction::kLeft:
-      pos_x -= speed;
+        new_x -= speed;
       break;
 
     case Direction::kRight:
-        pos_x += speed;
+        new_x += speed;
       break;
+    default:
+        break;
   }
 
-  // Wrap the Pacman around to the beginning if going off of the screen.
-  pos_x = fmod(pos_x + screen_width/width, screen_width/width);
-  pos_y = fmod(pos_y + screen_height/height, screen_height/height);
+  update = TryToMove(new_x, new_y);
+  if(update)
+  {
+      // Wrap the Pacman around to the beginning if going off of the screen.
+  	  pos_x = fmod(new_x + screen_width/width, screen_width/width);
+  	  pos_y = fmod(new_y + screen_height/height, screen_height/height);
+  }
 }
 
-// Inefficient method to check if cell is occupied by pacman.
-bool Pacman::PacmanCell(int x, int y) {
-  if (x == static_cast<int>(pos_x) && y == static_cast<int>(pos_y)) {
-    return true;
-  }
-  return false;
+// Check if a certain object intersects with pacman at certain cooardinate
+bool Pacman::PacmanIntersect(float objectPos1, float objectLen1, float objectPos2, float objectLen2) {
+  float objectStart1 = objectPos1 * objectLen1;
+  float objectEnd1 = (objectPos1+1) * objectLen1;
+  float objectStart2 = objectPos2 * objectLen2;
+  float objectEnd2 = (objectPos2+1) * objectLen2;
+  if((objectStart1>=objectStart2 && objectStart1<=objectEnd2) || (objectEnd1>=objectStart2 && objectEnd1<=objectEnd2)
+        || (objectStart2>=objectStart1 && objectEnd2<=objectEnd1))
+      return true;
+  else
+      return false;
 }
+
--- src/pacman.h
+++ src/pacman.h
@@ -2,17 +2,18 @@
 #define PACMAN_H
 
 #include "SDL.h"
+#include <vector>
 
 class Pacman {
  public:
   enum class Direction { kUp, kDown, kLeft, kRight, kStop };
 
-  Pacman(int screen_width, int screen_height)
+  Pacman(int screen_width, int screen_height, std::vector<SDL_Point> &wall)
       : screen_width(screen_width),
         screen_height(screen_height),
+        wall(wall),
         pos_x(0),
-        pos_y(0) {}
-  bool PacmanCell(int x, int y);
+        pos_y(31) {}
   void UpdatePosition();
 
   Direction direction = Direction::kUp;
@@ -21,13 +22,17 @@ class Pacman {
   float pos_x;
   float pos_y;
   const float width = 20;
-  const float height = 20; 
+  const float height = 20;
+  std::vector<SDL_Point> const &wall;
 
  private:
+  bool TryToMove(float x, float y);
+  bool PacmanIntersect(float objectPos1, float objectLen1, float objectPos2, float objectLen2);
 
   int screen_width;
   int screen_height;
 };
 
 #endif
+
 
